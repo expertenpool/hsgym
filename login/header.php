d7b2e45fcb3bdf335fb53f98ce346242eaed36ec
expertenpool <support@hsgym-expertenpool.ch>
2016-09-26 16:24:05 +0200

    Delete header.php

--- login/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-?>
-
-<div id="header_bg">
-		<div id="header_content" align="left" style="padding-top: 15px">
-			<a style="text-decoration: none;" href="./logout.php" target="_self"><img style="border: 0 none;" src="assets/logo.png"/></a> 
-		</div>
-</div>
